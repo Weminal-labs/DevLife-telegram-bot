b8a6c8c2936bec97bb5c804322d2a55936bb68b2
FucktheKingcode <coderhopham@gmail.com>
2024-08-22 12:54:58 +0700

    update: front end

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -21,12 +20,6 @@ export default defineConfig({
       define: {
         global: "globalThis",
       },
-      // Enable esbuild polyfill plugins
-      plugins: [
-        NodeGlobalsPolyfillPlugin({
-          buffer: true,
-        }),
-      ],
     },
   },
   server: {
